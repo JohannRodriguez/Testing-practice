5a93e380e9c72e36a8c8ad8cc22deb78c16c4d34
Lilith <themrblacksapce@hotmail.com>
2020-10-19 14:23:31 -0600

    Create capitalize function with testing

---
+++ unit-test.test.js
@@ -0,0 +1,9 @@
+const capitalize = require('./capitalize');
+
+it('capitalize when string is lowercase', () => {
+  expect(capitalize('lion')).toBe('Lion');
+});
+
+it('capitalize when string is uppercase', () => {
+  expect(capitalize('TURTLE')).toBe('Turtle');
+});
